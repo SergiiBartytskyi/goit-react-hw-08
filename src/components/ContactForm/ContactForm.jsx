ea94999fdfd66d8e397bf875dc16f860aa4cedc1
SergiiBartytskyi <s.bartycjkyj@gmail.com>
2024-09-17 12:17:00 +0200

    add new style

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,6 +4,7 @@ import { addContact, editContact } from "../../redux/contacts/operations";
 import { useId } from "react";
 import * as Yup from "yup";
 import toast, { Toaster } from "react-hot-toast";
+import clsx from "clsx";
 import css from "./ContactForm.module.css";
 
 const FeedbackSchema = Yup.object().shape({
@@ -11,7 +12,7 @@ const FeedbackSchema = Yup.object().shape({
   number: Yup.string()
     .matches(
       /^\d{3}-\d{2}-\d{2}$/,
-      "Неправильний формат номера телефону. Введіть у форматі 123-45-67"
+      "Incorrect phone number format. Enter in the format 123-45-67"
     )
     .min(9, "Too Short!")
     .max(9, "Too Long!")
@@ -72,7 +73,13 @@ const ContactForm = ({ initialValues, onFormSubmit }) => {
           />
         </div>
 
-        <button type="submit" className={css.formBtn}>
+        <button
+          type="submit"
+          className={clsx(css.formBtn, {
+            [css.editBtn]: initialValues.id,
+            [css.addBtn]: !initialValues.id,
+          })}
+        >
           {initialValues.id ? "Edit" : "Add Contact"}
         </button>
         <Toaster />
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,7 +4,7 @@ import css from "./Loader.module.css";
 const Loader = () => {
   return (
     <div className={css.loader}>
-      <BeatLoader color="orangered" />
+      <BeatLoader color="rgb(0 255 55)" />
     </div>
   );
 };
